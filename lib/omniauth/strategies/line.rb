68d484ab23ae06f5c61ec2c51daaa702607ed6f4
Tatsuki Sugiura <sugi@nemui.org>
2019-06-22 02:36:32 +0900

    Support friend_status_changed param filter for callback_url.

--- lib/omniauth/strategies/line.rb
+++ lib/omniauth/strategies/line.rb
@@ -20,10 +20,10 @@ module OmniAuth
       end
 
       def query_string
-        return super unless "1.4.0" <= OmniAuth::OAuth2::VERSION && OmniAuth::OAuth2::VERSION <= "1.6.0"
-        # Filter out 'code' & 'state' param to avoid redirect_url mismatch error with omniauth-oauth2 >= 1.4.0
-        return '' if request.params.keys.reject{|key| %w(code state).member?(key.to_s)}.empty?
-        "?" + request.query_string.gsub(/\b&?(code|state)=[^;&]*/, '')
+        return super if OmniAuth::OAuth2::VERSION < "1.4.0"
+        # Filter out 'code', 'state' and 'friend_status_changed' param to avoid redirect_url mismatch error with omniauth-oauth2 >= 1.4.0
+        return '' if request.params.keys.reject{|key| %w(code state friendship_status_changed).member?(key.to_s)}.empty?
+        "?" + request.query_string.gsub(/\b&?(code|state|friendship_status_changed)=[^;&]*/, '')
       end
 
       uid { raw_info['userId'] }
